a0550d07627b89f0675c6cf1f83071bf64b83564
José Victor <josevictornascimento2016@gmail.com>
2024-07-21 13:51:34 -0300

    feat: task update

--- src/main/java/br/com/josevictor/todolist/task/TaskController.java
+++ src/main/java/br/com/josevictor/todolist/task/TaskController.java
@@ -1,6 +1,5 @@
 package br.com.josevictor.todolist.task;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -9,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,5 +50,16 @@ public class TaskController {
     var tasks = this.taskRepository.findByUserId((UUID) userId);
 
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+    taskModel.setId(id);
+    taskModel.setCreatedAt(LocalDateTime.now());
+
+    this.taskRepository.save(taskModel);
+    return ResponseEntity.status(HttpStatus.OK).body(taskModel);
   }
 }
