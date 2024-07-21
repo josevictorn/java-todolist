25385921a26848cc16dbf236193bfdb3de05cad0
José Victor <josevictornascimento2016@gmail.com>
2024-07-21 12:27:03 -0300

    feat: create task and create basic authentication filter

---
+++ src/main/java/br/com/josevictor/todolist/task/TaskController.java
@@ -0,0 +1,27 @@
+package br.com.josevictor.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+  
+  @Autowired
+  private ITaskRepository taskRepository;
+
+  @PostMapping("/")
+  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = request.getAttribute("userId");
+    taskModel.setUserId((UUID) userId);
+    var task = this.taskRepository.save(taskModel);
+    return task;
+  }
+}
